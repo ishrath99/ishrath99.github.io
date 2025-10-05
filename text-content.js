b5078ab377e2979c20f53d91d85f375ad0788bbd
Ishrath Ahamed <Ishrath.Ahamed@cloudsolutions.com.sa>
2025-10-05 12:48:50 +0530

    update text content

--- text-content.js
+++ text-content.js
@@ -4,6 +4,6 @@ const textContent = {
     heroDescription: "It is important to draw wisdom from many different places. If you take it from only one place, it becomes rigid and stale.<br>- <i>Uncle Iroh</i>",
     aboutText1: "I’m a full-stack developer with an interdisciplinary background that bridges Biomedical Engineering, AI, and software systems. Currently, I work at Cloud Solutions International, where I design and develop scalable microservices, automate releases, and manage Kubernetes-based deployments. I enjoy solving complex problems by connecting ideas across domains",
     aboutText2: "Outside of work, you’ll often find me practicing calisthenics, playing soccer with friends, or writing short stories and exploring random new skills.",
-    contactText: "If you’d like to collaborate, share ideas, or discuss how technology can make things smarter and simpler, feel free to reach out. Always happy to connect with fellow creators and curious thinkers.",
+    contactText: "If you’d like to collaborate, share ideas, or discuss how technology can make things smarter and simpler, feel free to reach out.",
     footerText: "&copy; 2025 | Thanks to Coding Agents for bringing this portfolio to life."
 };
