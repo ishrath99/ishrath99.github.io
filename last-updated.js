667764adf7294abd46f67c43e8e1c7af2a72d157
Ishrath Ahamed <Ishrath.Ahamed@cloudsolutions.com.sa>
2025-12-06 14:12:52 +0530

    feat: Add new project entry for computational microscope and refine TwinDoctor description.

--- last-updated.js
+++ last-updated.js
@@ -1,1 +1,1 @@
-const lastUpdated = 'Sat Dec  6 02:03:04 PM +0530 2025';
+const lastUpdated = 'Sat Dec  6 02:12:52 PM +0530 2025';
--- projects-data.js
+++ projects-data.js
@@ -3,11 +3,21 @@ const projects = [
     {
         "icon": "assets/projects/twindoctor-logo.png",
         "title": "TwinDoctor",
-        "description": `An intelligent platform integrating agentic orchestration and MCP tools with graph-based knowledge retrieval for the healthcare domain. This secure system leverages Neo4j and RAG to drive complex workflows, with contributions to the open-source Agno library.`,
+        "description": `An intelligent platform integrating agentic orchestration and MCP tools with graph-based knowledge retrieval for the healthcare domain. This system leverages Neo4j and RAG to drive complex workflows, with contributions to the open-source Agno library.`,
         "tags": ["Python", "Neo4j", "MCP", "A2A", "RAG", "Agno"],
         "links": [
             { "icon": "fab fa-github", "text": "Agno", "href": "https://github.com/agno-agi/agno" },
             { "icon": "fas fa-video", "text": "Live Demo", "href": "https://drive.google.com/file/d/1jdiEFrAjf5XqVifuVcPCzHEG9C3fByJt/view?usp=drive_link" }
+        ]
+    },
+    {
+        "icon": "assets/projects/fyp-logo.png",
+        "title": "Custom computational microscope design and 3D image reconstruction",
+        "description": "Designed a novel physics-inspired 3D imaging system using structured illumination to encode depth information onto a planar detector. Developed a computational reconstruction framework integrating deep learning and prior manifolds to solve the inverse problem, enabling 3D image recovery from compressive measurements.",
+        "tags": ["Python", "MATLAB", "pytorch"],
+        "links": [
+            { "icon": "fab fa-github", "text": "github", "href": "https://github.com/sanjith1999/FRIS" },
+            { "icon": "fas fa-file-pdf", "text": "Demo Poster", "href": "https://drive.google.com/file/d/1_ik1lRWBjjXKde-qMYyjJDIQfq5x0QnQ/view?usp=drive_link" }
         ]
     },
     {
